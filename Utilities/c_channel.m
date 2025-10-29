7887057fc4b79b7b00740a5cd2bf9c0c52487e97
Simon Tarboush <36705758+SimonTarboush@users.noreply.github.com>
2025-10-30 00:42:02 +0300

    Update c_channel.m

--- Utilities/c_channel.m
+++ Utilities/c_channel.m
@@ -7,12 +7,12 @@
 % "Mutual Coupling-Aware Channel Estimation and Beamforming for RIS-Assisted Communications", 
 % accepted by IEEE Transactions on Wireless Communications.
 % *: P. Zheng and S. Tarboush are co-first authors; they contributed equally to this paper and the implementation of these codes.
-% IEEE-Xplore: https://ieeexplore.ieee.org/document/?????
+% IEEE-Xplore: https://ieeexplore.ieee.org/document/11176921
 % pre-print ArXiV: https://arxiv.org/abs/2410.04110
 %
-% Codes are also available on IEEE-Xplore: (will link the codes to CodeOcean)
+% Codes are also available on IEEE-Xplore:
 % GitHub: https://github.com/SimonTarboush/Mutual-Coupling-Aware-Channel-Estimation-and-Beamforming-for-RIS-Assisted-Communications
-%              https://github.com/ZPinjun/????
+%              https://github.com/ZPinjun/MC-channel-estimation
 %
 % Contact person email: simon.tarboush@tu-berlin.de && pinjun.zheng@ubc.ca
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
